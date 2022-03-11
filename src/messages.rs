e54fcbe954c18bb6bbe7474cb04655effb123251
Sébastien LUNDI <slundi@gmail.com>
2022-03-11 11:10:25 +0100

    Remove unused

--- src/messages.rs
+++
@@ -1,8 +0,0 @@
-use serde::Deserialize;
-
-#[derive(Deserialize, Debug)]
-pub struct ConfigurationClient {pub client: String,}
-#[derive(Deserialize, Debug)]
-pub struct ConfigurationMinUploadSpeed {pub min_upload_speed: u16,}
-#[derive(Deserialize, Debug)]
-pub struct ConfigurationMaxUploadSpeed {pub max_upload_speed: u16,}
