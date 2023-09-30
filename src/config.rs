ad68aa7302a8d8ba8471147b5c7530c753aed7ff
Sébastien LUNDI <slundi@gmail.com>
2023-09-30 17:40:09 +0200

    global client

--- src/config.rs
+++ src/config.rs
@@ -1,14 +1,17 @@
-use serde::{Serialize, Deserialize};
 use rand::Rng;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Config {
     /// Server `<IP or hostaname>:<port>`. Default is `127.0.0.1:8070`
-    #[serde(skip_serializing)] pub server_addr: String,
+    #[serde(skip_serializing)]
+    pub server_addr: String,
     /// Log level (available options are: INFO, WARN, ERROR, DEBUG, TRACE). Default is `INFO`.
-    #[serde(skip_serializing)] pub log_level: String,
+    #[serde(skip_serializing)]
+    pub log_level: String,
     /// torrent port
-    #[serde(skip_serializing)] pub port: u16,
+    #[serde(skip_serializing)]
+    pub port: u16,
     pub min_upload_rate: u32,   //in byte
     pub max_upload_rate: u32,   //in byte
     pub min_download_rate: u32, //in byte
@@ -16,10 +19,13 @@ pub struct Config {
     //pub simultaneous_seed: u16, //useful ?
     pub client: String,
     /// Directory where torrents are saved
-    #[serde(skip_serializing)] pub torrent_dir: String,
+    #[serde(skip_serializing)]
+    pub torrent_dir: String,
     /// Set a custom web root (ex: / or /ratio-up/)
-    #[serde(skip_serializing)] pub web_root: String,
-    #[serde(skip_serializing)] pub key_refresh_every: u16,
+    #[serde(skip_serializing)]
+    pub web_root: String,
+    #[serde(skip_serializing)]
+    pub key_refresh_every: u16,
 }
 impl Default for Config {
     fn default() -> Self {
@@ -39,5 +45,42 @@ impl Default for Config {
             key_refresh_every: 0,
             client: String::from("INVALID"),
         }
+    }
+}
+impl Config {
+    /// Load configuration in environment. Also load client.
+    pub fn load_config() -> Config {
+        let mut config: Config = Config::default();
+        for (key, value) in std::env::vars() {
+            if key == "SERVER_ADDR" {
+                config.server_addr = value.clone();
+            }
+            if key == "LOG_LEVEL" {
+                config.log_level = value.clone();
+            }
+            if key == "MIN_UPLOAD_RATE" {
+                config.min_upload_rate = value.clone().parse::<u32>().expect("Wrong upload rate");
+            }
+            if key == "MAX_UPLOAD_RATE" {
+                config.max_upload_rate = value.clone().parse::<u32>().expect("Wrong upload rate");
+            }
+            if key == "MIN_DOWNLOAD_RATE" {
+                config.min_download_rate =
+                    value.clone().parse::<u32>().expect("Wrong download rate");
+            }
+            if key == "MAX_DOWNLOAD_RATE" {
+                config.max_download_rate =
+                    value.clone().parse::<u32>().expect("Wrong download rate");
+            }
+            if key == "CLIENT" {
+                config.client = value.clone();
+            }
+            if key == "TORRENT_DIR" {
+                config.torrent_dir = value.clone();
+            }
+        }
+        // let client = &mut *CLIENT.write().expect("Cannot get client");
+        // client.build(clients::ClientVersion::from_str(&config.client).expect("Wrong client"));
+        config.clone()
     }
 }
